36b423db7ebe382aa277a92d3e41ee1bbcf0a3ac
minhkhoa998832 <164452997+minhkhoa998832@users.noreply.github.com>
2025-09-02 18:43:12 +0700

    Delete Doantotnghiep_ATTT11-main directory

--- Doantotnghiep_ATTT11-main/frontend/src/services/authService.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-const API = process.env.REACT_APP_API_URL + '/auth';
-
-export const login = (data) => axios.post(`${API}/login`, data);
-export const register = (data) => axios.post(`${API}/register`, data);
